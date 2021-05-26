bb8d2651fea618a1d6f6eb0dec1897db662773cc
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-25 17:56:27 -0700

    router changes deleted ahead of merge

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,20 +1,6 @@
-// import React from 'react';
-// import artistSearch from '../../containers/artistSearch';
-
-// export default function App() {
-//   return <artistSearch />;
-// }
-
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import AlbumDetails from '../../containers/AlbumDetails';
+import artistSearch from '../../containers/artistSearch';
 
 export default function App() {
-  return (
-    <Router>
-            
-      <Route exact path="/:releaseId" component={AlbumDetails} />
-          
-    </Router>
-  );
+  return <artistSearch />;
 }
