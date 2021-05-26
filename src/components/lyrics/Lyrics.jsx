6142cdd0ad45b2a30e7f94366c96bce8f9b74ded
Minh Ngo <ngo.minh0224@gmail.com>
2021-05-26 13:58:59 -0700

    some styling lyrics css

--- src/components/lyrics/Lyrics.jsx
+++ src/components/lyrics/Lyrics.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Lyrics.css';
 
 export default function Lyrics({ lyricsArray }) {
   return <article>{lyricsArray}</article>;
