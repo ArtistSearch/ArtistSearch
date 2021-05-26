ed660dcc404205b77091ea37bf63d3a300fad47c
Minh Ngo <ngo.minh0224@gmail.com>
2021-05-25 21:57:42 -0700

    lyrics rendering

--- src/containers/artistDetails.jsx
+++ src/containers/artistDetails.jsx
@@ -1,53 +1,5 @@
-/* eslint-disable indent */
-/* eslint-disable max-len */
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import Release from '../components/app/Release';
-import { getReleases } from '../services/getArtists';
+import React from 'react';
 
 export default function artistDetails() {
-  const [loading, setLoading] = useState(true);
-  const [releases, setReleases] = useState(null);
-  const [page, setPage] = useState(1);
-
-  const { artistName } = useParams();
-  const { artistId } = useParams();
-
-  useEffect(() => {
-    getReleases(artistId)
-      .then(setReleases)
-      .finally(() => setLoading(false));
-  }, [page]);
-
-  const totalPages = '25?';
-
-  const onPageDown = () => {
-
-  };
-
-  const onPageUp = () => {
-
-  };
-
-  if (loading) return (
-    <h1>Loading...</h1>
-  );
-
-  return (
-    <div>
-      <button onClick={onPageDown} disabled={page <= 1}>
-        previous page
-    </button>
-      <span>
-        {page} / {totalPages}
-      </span>
-      <button onClick={onPageUp} disabled={page >= totalPages}>
-        next page
-    </button>
-      <h1>
-        Releases by {artistName}
-      </h1>
-      {releases.map((release) => <Release key={release.id} {...release} artistName={artistName} />)}
-    </div>
-  );
+  return <div></div>;
 }
